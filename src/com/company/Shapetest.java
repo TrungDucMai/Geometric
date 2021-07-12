c22e7d8ac4ddd06b1525c2b692ec58a1a5429c46
TrungDucMai <trungmaiduc196@gmail.com>
2021-07-12 10:47:40 +0700

    'taomoi;

---
+++ src/com/company/Shapetest.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Shapetest {
+    public static void main(String[] args){
+        Shape shape = new Shape();
+        System.out.println(shape);
+        shape = new Shape( "red", false);
+        System.out.println(shape);
+    }
+
+
+}
